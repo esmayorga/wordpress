d75164abfbcdcc9e0a845120352d234c037ed200
Sebastian <mayorga.sebas100@hotmail.com>
2020-03-12 09:25:40 -0500

    Agregando el menu

--- functions.php
+++ functions.php
@@ -3,6 +3,14 @@ function init_template() {
     add_theme_support( 'post-thumbnails' );
     add_theme_support( 'title-tag' );
 
+    // Anadiendo la parte del menu
+
+    register_nav_menus(
+        array(
+            'top_menu' => 'Menú Principal'
+        )
+    );
+
 }
 
 add_action( 'after_setup_theme', 'init_template' );
@@ -10,14 +18,16 @@ add_action( 'after_setup_theme', 'init_template' );
 function assets(){
     // Registrar los stylos
     wp_register_style('bootstrap', "https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css", '','4.4.1', 'all');
-    
     wp_register_style('montserrat', "hhttps://fonts.googleapis.com/css?family=Montserrat&display=swap", '','1.0', 'all');
-
     // Para referenciar productos.
     wp_enqueue_style('estilos', get_stylesheet_uri(), array('bootstrap','montserrat'),'1.0', 'all');
+
+    // Agregando archivos javascrip
+  
+    wp_enqueue_script('custom', get_template_directory_uri().'/assets/js/custom.js', '', '1.0', true);
 
 
 }
 
-
+// Ejecutando la funcion de Assets
 add_action('wp_enqueue_scripts','assets');
--- header.php
+++ header.php
@@ -6,5 +6,29 @@
     <title> Aeroclub Guaymaral </title>
     <?php wp_head()  ?>
 </head>
+<header>
+    <div class="container">
+        <div class="row align-items-center">
+            <div class="col-4">
+                <img src="<?php echo get_template_directory_uri()?>/assets/img/logo.png" alt="Logo de Aeroclub Guaymaral" width="120px">  
+             </div>
+             <div class="col-8" >
+                <nav>
+                    <?php wp_nav_menu(
+                        array(
+                            'theme_location' => 'top_menu',
+                            'menu_class'    => 'menu-principal',
+                            'container_class' => 'container-menu',
+                        )
+                        ); 
+                    ?>
+                </nav>
+        </div>
+
+    </div>
+
+
+</header>
 <body>
+
     
