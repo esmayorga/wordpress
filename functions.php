0836a78f64ffaddba8be11eec9cc7bde3a8b63e0
Sebastian <mayorga.sebas100@hotmail.com>
2020-03-12 08:28:26 -0500

    Correcion de llamados insertando los css en wordpress

--- functions.php
+++ functions.php
@@ -9,8 +9,9 @@ add_action( 'after_setup_theme', 'init_template' );
 
 function assets(){
     // Registrar los stylos
-    wps_register_style('bootstrap', "https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css", '','4.4.1', 'all');
-    wps_register_style('montserrat', "hhttps://fonts.googleapis.com/css?family=Montserrat&display=swap", '','1.0', 'all');
+    wp_register_style('bootstrap', "https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css", '','4.4.1', 'all');
+    
+    wp_register_style('montserrat', "hhttps://fonts.googleapis.com/css?family=Montserrat&display=swap", '','1.0', 'all');
 
     // Para referenciar productos.
     wp_enqueue_style('estilos', get_stylesheet_uri(), array('bootstrap','montserrat'),'1.0', 'all');
@@ -18,4 +19,5 @@ function assets(){
 
 }
 
-add_action('wp_enqueue_style', 'assets');
+
+add_action('wp_enqueue_scripts','assets');
