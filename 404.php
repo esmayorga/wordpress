d554d2da58f9dbd8637a950f22738d7f2f51675f
Sebastian <mayorga.sebas100@hotmail.com>
2020-03-13 14:52:59 -0500

    Error 404

---
+++ 404.php
@@ -0,0 +1,9 @@
+<?php get_header(  ) ?>
+<main class="container">
+    <div class="pagina404 my-5">
+        <h1> Error 404 Pagina no encontrada </h1>
+        <h2> haga <a href="<?php echo home_url(); ?>"> Clic aqui </a> Para Volver a la pagina principal <h2>
+    </div>
+    
+</main>
+<?php get_footer(  ) ?>
